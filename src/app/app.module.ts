7c42a4eea50387a86b563e880af847400d47b4b6
romantut1988 <romantut1988@mail.ru>
2022-12-18 02:36:23 +0300

    use ng container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import { ChildComponent } from './parent/child/child.component'
+import { ParentComponent } from './parent/parent.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ChildComponent, ParentComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/parent/child/child.component.ts
@@ -0,0 +1,18 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'insta-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.scss']
+})
+export class ChildComponent {
+
+  @Output() sendGradeEvent = new EventEmitter<string>()
+
+  inputGrade = ''
+
+
+  sendGradeHandler() {
+    this.sendGradeEvent.emit(this.inputGrade)
+  }
+}
