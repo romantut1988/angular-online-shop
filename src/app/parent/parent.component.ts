241084ed5d378f16d5803c3b50c4910b8e871dbd
romantut1988 <romantut1988@mail.ru>
2022-12-21 23:56:41 +0300

    we use ngswitch

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,69 +1,11 @@
 import {Component} from '@angular/core';
 
-interface WeekGrade {
-  id: number
-  gradeItem: number
-}
-
-interface Lesson {
-  id: number
-  title: string
-  weekGrades: WeekGrade[]
-}
-
 @Component({
   selector: 'insta-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss']
 })
+
 export class ParentComponent {
-  isLoading = false
-  lessons: Lesson[] = [
-    {
-      id: 0,
-      title: 'Math',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 5
-        },
-        {
-          id: 1,
-          gradeItem: 2
-        },
-        {
-          id: 2,
-          gradeItem: 3
-        },
-      ]
-    },
-    {
-      id: 1,
-      title: 'Physic',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 3
-        },
-        {
-          id: 1,
-          gradeItem: 4
-        },
-        {
-          id: 2,
-          gradeItem: 9
-        },
-      ]
-    }
-  ]
-
-  constructor() {
-    setTimeout(() => {
-      this.isLoading = false
-    }, 3000)
-  }
-
-  getGrade(grade: string) {
-    //   this.grades.push(grade)
-  }
+  value = ''
 }
