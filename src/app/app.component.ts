a58e06413dd86c35d3b29e83a287e65957783d30
romantut1988 <romantut1988@mail.ru>
2023-01-04 23:59:24 +0300

    restart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'insta-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appTitle = 'Instagram'
-  text = 'start value'
+  title = 'To Do List';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {FormsModule} from '@angular/forms';
-import { ChildComponent } from './parent/child/child.component'
-import { ParentComponent } from './parent/parent.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, ChildComponent, ParentComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
-
+export class AppModule { }
