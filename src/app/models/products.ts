ed883191c06537bf58b30279ab574162973fdb68
romantut1988 <romantut1988@mail.ru>
2023-01-05 15:52:23 +0300

    transition to the main page fixim

---
+++ src/app/models/products.ts
@@ -0,0 +1,15 @@
+export interface IProducts {
+  id: number;
+  title: string;
+  price: number;
+  year: string
+  image?: string;
+  configure: IProductsConfig;
+}
+
+export interface IProductsConfig {
+  chip: string;
+  ssd: string;
+  memoty: string;
+  display: string;
+}
