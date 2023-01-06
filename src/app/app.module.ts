19c9289d489429d7cf4f533d486ccb9f8b9b93bc
romantut1988 <romantut1988@mail.ru>
2023-01-07 00:55:58 +0300

    start start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/UI/footer/footer.component';
-import { HeaderComponent } from './components/UI/header/header.component';
-import { ProductsComponent } from './components/products/products.component';
-import { BasketComponent } from './components/basket/basket.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { BaseComponent } from './components/base/base.component';
-import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    ProductsComponent,
-    BasketComponent,
-    ProductDetailsComponent,
-    BaseComponent,
-    DialogBoxComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    HttpClientModule,
-    MatDialogModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
