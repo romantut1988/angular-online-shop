8ad2885dbc84a9862bc768915ea29c0cdc18671e
romantut1988 <romantut1988@mail.ru>
2023-01-07 01:52:41 +0300

    interpolation method apply

---
+++ src/app/date/date.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-date',
+  templateUrl: './date.component.html',
+  styleUrls: ['./date.component.scss'],
+})
+export class DateComponent {
+  message: string = 'Hello World';
+  constructor() {}
+}
